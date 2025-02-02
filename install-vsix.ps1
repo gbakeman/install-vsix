d649048dabb0edac3d1df901971f3a20e9da2b52
gbakeman <gbakeman@live.com>
2025-02-02 16:36:13 -0500

    Live log, process monitoring, error handling
    - Use FSWatcherEngineEvent to monitor the temp directory for logs that are written by VSIXInstaller, and print them out in (near) real time as allowed by the writer. This is configurable by the user through previously added argument.
    - Because we returned the process object earlier, the script can now sit in a loop waiting until the main VSIXInstaller process exits. This allows for
      - Regular monitoring of the Process object (shows information such as CPU use)
      - Monitoring of desired log files for printing directly to the console (hopefully to help with long install times)
    - Updated cleanup code accordingly
    - Check if VSIXInstaller exited with an abnormal exit code, and if so, find any exceptions and print them out
    - Also avoid exiting the terminal if not in a github workspace

--- install-vsix.ps1
+++ install-vsix.ps1
@@ -47,10 +47,92 @@ if (-Not (Test-Path $VsixLocation)) {
 }
 Write-Host "VSInstallDir is $($VSInstallDir)"
 Write-Host "VsixLocation is $($VsixLocation)"
+
+Write-Host "Initializing log file monitoring..."
+Install-Module -Name FSWatcherEngineEvent
+# Date format is yyyyMMddHHmmss
+$LogFileNameIncludes = @( "dd_VSIXInstaller_*" )
+if ($OutputDetailedInfo) { $LogFileNameIncludes += "dd_setup*.log" }
+else { $LogFileNameIncludes += "dd_setup_*_errors.log" }
+$LogFileReaders = @{}
+
+function FSWatcherEventAction($FSWEvent) {
+    $FileName = $FSWEvent.MessageData.Name
+    Write-Debug "File event: $($FileName)"
+
+    foreach ($item In $LogFileNameIncludes) {
+        if (($FileName -like $item) -and (!$LogFileReaders.ContainsKey($FileName))) {
+            Write-Debug "Returning new matching and unopened file."
+            return $FileName
+        }
+    }
+}
+
+Write-Host "Connecting file monitor event handler..."
+# https://github.com/wgross/fswatcher-engine-event/blob/main/README.md
+$WatcherJob = New-FileSystemWatcher -SourceIdentifier "VSIXLogFileMonitor" -Path $VsixDir -Filter "dd_*.log" -Action { FSWatcherEventAction $event }
+
 Write-Host "Installing $PackageName..."
 $proc = Start-Process -Filepath "$($VSInstallDir)\VSIXInstaller" -ArgumentList "/q $($VsixLocation)" -PassThru
 
-Write-Host "Cleanup..."
-rm $VsixLocation
+while ($proc.HasExited -eq $false) {
+    # Check if the watcher job has encountered an error and pass it along
+    if ($WatcherJob.JobStateInfo.State.HasFlag([System.Management.Automation.JobState]::Failed)) {
+        throw $WatcherJob.Error[0].Exception
+    }
 
-Write-Host "Installation of $PackageName complete!"
+    # Check if new file to watch was returned
+    $watcherJobOut = Receive-Job -Job $WatcherJob
+    if ($watcherJobOut) {
+        Write-Debug "Received output from watcher job!" ; $watcherJobOut
+        # Sometimes an array might be returned (works for single objects too)
+        $watcherJobOut | ForEach-Object {
+            if (!($LogFileReaders.ContainsKey($_))) {
+                # Open file for reading, using a workaround that gives us the most permissive sharing & access.
+                $LogFileReaders[$_] = New-Object System.IO.StreamReader ( New-Object System.IO.FileStream(
+                        "$($VsixDir)\$($_)", [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite))
+                Write-Host "Opened $($_) for reading."
+            }
+            else { Write-Debug "Attempted to open a duplicate streamreader!"}
+        }
+    }
+
+    # Iterate through all streamreaders and read-out to the end of the buffer
+    $LogFileReaders.GetEnumerator() | ForEach-Object {
+        if (!$_.Value.EndOfStream) {
+            Write-Host "[$($_.Key)] bytes $($_.Value.BaseStream.Position) to $($_.Value.BaseStream.Length)"
+            while (!$_.Value.EndOfStream) { Write-Host $_.Value.ReadToEnd() }
+            Write-Host "[End of stream]`r`n"
+        }
+    }
+
+    if ($MonitorProcessInfo) { Write-Host ($proc | Format-Table | Out-String) }
+
+    Start-Sleep 1
+}
+
+Write-Host "Process exited."
+
+Write-Host "Cleanup..."
+
+Remove-FileSystemWatcher -SourceIdentifier "VSIXLogFileMonitor"
+Remove-Item $VsixLocation
+$LogFileReaders.GetEnumerator() | ForEach-Object {
+    Write-Debug "Closing file $($_.Key)"
+    $_.Value.Close()
+}
+
+if ($proc.ExitCode -ne 0) {
+    Write-Host "Error encountered: $($proc.ExitCode)"
+    Write-Host (Select-String -Path $env:temp\dd_setup_*_errors.log -Pattern "^.*Exception.*$")
+}
+else {
+    Write-Host "Installation of $PackageName complete!"
+}
+
+if ($env:GITHUB_WORKSPACE) {
+    exit $proc.ExitCode
+}
+else {
+    Write-Host "Simulated exit code $($proc.ExitCode)"
+}
